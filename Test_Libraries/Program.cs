0a4cd75593d27a6e5f840d78358fef7ba6cb7e09
Andrey Seredin <fliby4@ya.ru>
2021-10-12 22:25:57 +0300

    Тестирование HtmlAgilityPack

--- Test_Libraries/Program.cs
+++ Test_Libraries/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 
 namespace Test_Libraries
 {
     class Program
     {
+        public const string URL = "https://simbirsoft.com";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HtmlAgilityPack_test(URL);
+        }
+
+        static private void AngleSharp_test(string URL)
+        {
+
+        }
+
+        static private void HtmlAgilityPack_test(string URL)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(URL);
+            var text = doc.DocumentNode.InnerText;
+            Console.WriteLine(text);
+            Console.ReadLine();
         }
     }
 }
